898a86b3a9592b8c232f646582e2c9dd259a148f
Abhi Agarwal <goldcobrapro@gmail.com>
2017-10-12 23:42:15 -0400

    Use libsass
    Removes the need for a local dependency while likely providing speedups.

--- source/main.py
+++ source/main.py
@@ -1,24 +1,25 @@
 """updates subreddit css with compiled sass"""
-import subprocess
 import time
 
+import sass
 import praw
+
 
 def css() -> str:
     """compiles sass and returns css"""
-    res: subprocess.CompletedProcess = subprocess.run(
-        "sass index.scss --style compressed --quiet", stdout=subprocess.PIPE
-        )
-    return res.stdout
+    return sass.compile(filename="index.scss", output_style="compressed")
+
 
 def uid() -> str:
     """return date and time"""
     return "Subreddit upload on {}".format(time.strftime("%c"))
 
+
 def update(reddit: praw.Reddit, style: str) -> None:
     """update subreddit stylesheet"""
     reddit.subreddit("neoliberal").stylesheet.update(style, reason=uid())
     return
+
 
 def main() -> None:
     """main function"""
@@ -26,6 +27,7 @@ def main() -> None:
     style: str = css()
     update(reddit, style)
     return
+
 
 if __name__ == '__main__':
     main()
