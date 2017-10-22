7a98cd1c58985da9230ba5861731b6f252d2c611
Abhi Agarwal <goldcobrapro@gmail.com>
2017-10-22 19:27:14 -0400

    Check for changed files from webhook
    Prevents uploading everything, only the changed assets

--- source/update.py
+++ source/update.py
@@ -1,8 +1,12 @@
 """updates subreddit css with compiled sass"""
+import os
 import time
+from typing import List, Dict, Any, Tuple
 
-import sass
 import praw
+import sass
+
+WebhookResponse = Dict[str, Any] # pylint: disable=C0103
 
 
 def css() -> str:
@@ -14,7 +18,26 @@ def uid() -> str:
     """return date and time"""
     return "Subreddit upload on {}".format(time.strftime("%c"))
 
-def update() -> None:
+
+def changed_assets(data: WebhookResponse) -> Tuple[List[str], List[str]]:
+    """identifies changed files to upload by checking if any changed files are images"""
+    endings: List[str] = ["png", "jpg"]
+
+    head_commit: Dict[str, Any] = data["head_commit"]
+
+    uploading_files: List[str] = [
+        file for file in (head_commit["modified"] + head_commit["added"])
+        for ending in endings
+        if os.path.splitext(file)[1] == ending
+        ]
+    removed_files: List[str] = [
+        file for file in head_commit["removed"]
+        for ending in endings
+        if os.path.splitext(file)[1] == ending
+    ]
+    return (uploading_files, removed_files)
+
+def update(data: WebhookResponse) -> None:
     """main function"""
     reddit: praw.Reddit = praw.Reddit()
     reddit.subreddit("neoliberal").stylesheet.update(css(), reason=uid())
