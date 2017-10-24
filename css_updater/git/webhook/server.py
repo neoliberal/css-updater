e22b3122808d811849c83554948363016e7ae3be
Abhi Agarwal <goldcobrapro@gmail.com>
2017-10-24 19:39:57 -0400

    Update server

--- css_updater/git/webhook/server.py
+++ css_updater/git/webhook/server.py
@@ -1,12 +1,19 @@
 """main server to listen to webhooks"""
 import json
-from typing import Dict, Any
+from typing import Dict, Any, TypeVar
 
-from flask import Flask, request
+from flask import Flask, request, abort
+
+# leave my typedefs alone, pylint: disable=C0103
+Key = TypeVar("Key")
+WebhookData = Dict[str, Key]
 
 app: Flask = Flask(__name__)
 
-@app.route('/', methods=["POST"])
-def recieve() -> None:
+@app.route('/webhook', methods=["POST"])
+def webhook() -> None:
     """recieve data and pass it to update"""
-    data: Dict[str, Any] = json.loads(request.data)
+    if request.methods == "POST":
+        data: WebhookData[Any] = json.loads(request.data)
+    else:
+        abort(400)
