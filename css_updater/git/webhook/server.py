584cbfa30b68a63cfe821a5d22f80a8d061a4945
Abhi Agarwal <goldcobrapro@gmail.com>
2017-10-24 20:20:40 -0400

    Add header verification
    Not working yet

--- css_updater/git/webhook/server.py
+++ css_updater/git/webhook/server.py
@@ -10,10 +10,20 @@ WebhookData = Dict[str, Key]
 
 app: Flask = Flask(__name__)
 
+def verify(git_hash: str, local_key: str) -> bool:
+    """verifies that git string is the same as the local string"""
+    import hmac
+    from hashlib import sha1
+    local_hash: str = "sha1=" + hmac.new(str.encode(local_key), digestmod=sha1).hexdigest()
+    return hmac.compare_digest(git_hash, local_hash)
+
 @app.route('/webhook', methods=["POST"])
 def webhook() -> None:
     """recieve data and pass it to update"""
     if request.methods == "POST":
+        if not verify(request.headers["X-Hub-Signature"], ""):
+            abort(400)
+
         data: WebhookData[Any] = json.loads(request.data)
     else:
         abort(400)
