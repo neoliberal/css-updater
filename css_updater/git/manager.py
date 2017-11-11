85711ceb4b716b81b91caf0e093dc0f4bd0c0179
Abhi Agarwal <goldcobrapro@gmail.com>
2017-11-11 16:14:23 -0500

    Cleanup Manager
    Remove config_handler, it's pretty unnecessary

--- css_updater/git/manager.py
+++ css_updater/git/manager.py
@@ -1,11 +1,11 @@
 """manages github repos"""
 import os
 import tempfile
+from typing import Dict, Any
 
 import pygit2 as git
 
 from .webhook.handler import Handler
-from .config_handler import ConfigHandler
 
 
 class Manager(object):
@@ -14,27 +14,12 @@ class Manager(object):
     def __init__(self: Manager, handler: Handler) -> None:
         self.webhook_handler: Handler = handler
         self.temp_dir: tempfile.TemporaryDirectory = tempfile.TemporaryDirectory()
-        self.repo: git.Repository = self.get_repo()
-        self.config: ConfigHandler = self.get_config()
+        self.repo: git.Repository = git.clone_repository(
+            self.webhook_handler.git_url, path=self.temp_dir.name)
+
+        with open(os.path.join(self.temp_dir.name, "css-updater.json")) as config:
+            import json
+            self.config: Dict[str, Any] = json.loads(config.read())
 
     def __del__(self: Manager) -> None:
         self.temp_dir.cleanup()
-
-    def get_repo(self: Manager) -> git.Repository:
-        """clone or initialize repository"""
-
-        return git.clone_repository(
-            self.webhook_handler.git_url, path=self.temp_dir.name)
-
-    def get_config(self: Manager) -> ConfigHandler:
-        """gets config file inside repo"""
-        repo_index: git.repository.Index = self.repo.index
-        repo_index.read()
-        try:
-            config_entry = repo_index["css_updator.json"]
-        except KeyError:
-            print("no config file exists")
-        else:
-            with self.repo[config_entry.id] as blob:
-                return ConfigHandler(blob.data)
-        return ConfigHandler("")
